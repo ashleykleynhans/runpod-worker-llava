4fb8b6e306e94580f584c1c55b81cc4856cefcf6
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-10-25 12:40:53 +0200

    Remove ugly hack

--- start_network_volume.sh
+++ start_network_volume.sh
@@ -4,11 +4,10 @@ echo "Worker Initiated"
 
 echo "Symlinking files from Network Volume"
 ln -s /runpod-volume /workspace
-rm -rf /root/.cache
-ln -s /runpod-volume/.cache /root/.cache
 
 echo "Starting RunPod Handler"
 export PYTHONUNBUFFERED=1
+export HF_HOME="/runpod_volume"
 source /workspace/runpod-worker-llava/venv/bin/activate
 cd /workspace/runpod-worker-llava
 python -u rp_handler.py
--- start_standalone.sh
+++ start_standalone.sh
@@ -2,12 +2,9 @@
 
 echo "Worker Initiated"
 
-echo "Symlinking files from Network Volume"
-rm -rf /root/.cache
-ln -s /workspace/.cache /root/.cache
-
 echo "Starting RunPod Handler"
 export PYTHONUNBUFFERED=1
+export HF_HOME="/workspacex"
 source /workspace/runpod-worker-llava/venv/bin/activate
 cd /workspace/runpod-worker-llava
 python -u rp_handler.py
