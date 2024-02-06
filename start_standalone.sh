2b9a3ca511c0fa68f87aa8e70b949d10ae2df280
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-02-06 19:48:37 +0200

    Use venv

--- start_standalone.sh
+++ start_standalone.sh
@@ -5,5 +5,6 @@ echo "Worker Initiated"
 echo "Starting RunPod Handler"
 export PYTHONUNBUFFERED=1
 export HF_HOME="/workspace"
+source /workspace/venv/bin/activate
 cd /workspace/runpod-worker-llava/src
 python3 -u rp_handler.py
